f3330f202beadf07f5ca8bb16168d4b43868c56c
dilumd <dilumda@gmail.com>
2016-11-19 00:53:06 +0530

    f

---
+++ logs/error.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-05-24 07:09:19 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-05-24	07:09:19	INFO	::1	Joomla FAILURE: 	Empty password not allowed
+2016-11-14	19:05:21	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-14	21:02:22	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	08:34:56	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	08:35:16	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	08:35:22	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	08:35:33	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	11:02:03	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	12:50:13	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	15:39:18	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
